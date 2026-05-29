57971b298e063327b81cad9aedcf8cd2f62760a2
maulinadwi <maulinadwi04@gmail.com>
2026-05-29 17:20:15 +0700

    commit pertama

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
